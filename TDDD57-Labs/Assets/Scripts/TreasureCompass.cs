399f6a48112482f23dbd8c391e8aac81de13e787
Axel Nordanskog <axeno840@student.liu.se>
2019-03-12 17:33:25 +0100

    Add treasure compass to ship front

---
+++ TDDD57-Labs/Assets/Scripts/TreasureCompass.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureCompass : MonoBehaviour
+{
+    public GameObject needle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Find closest treasure
+        var treasures = GameObject.FindObjectsOfType<Treasure>();
+        Transform closestTreasure = null;
+        float closestDistance = float.MaxValue;
+        foreach (Treasure treasure in treasures)
+        {
+            float distance = Vector3.Distance(transform.position, treasure.transform.position);
+            if (distance < closestDistance)
+            {
+                closestTreasure = treasure.transform;
+                closestDistance = distance;
+            }
+        }
+
+        // Turn needle
+        if (closestTreasure != null)
+        {
+            var toTreasure = closestTreasure.position - needle.transform.position;
+            if (toTreasure.x != 0.0f || toTreasure.z != 0.0f)
+            {
+                needle.transform.right = Vector3.Scale(toTreasure, new Vector3(1.0f, 0.0f, 1.0f));
+            }
+        }
+    }
+}
