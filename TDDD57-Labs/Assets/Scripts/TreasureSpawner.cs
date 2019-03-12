72aad8227004054799f4c1c41caee91bf62c4bcc
Axel Nordanskog <axeno840@student.liu.se>
2019-03-12 20:39:38 +0100

    Spawn treasures at random

---
+++ TDDD57-Labs/Assets/Scripts/TreasureSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureSpawner : MonoBehaviour
+{
+    public GameObject treasurePrefab;
+
+    private int treasureCount = 10;
+    
+    void Start()
+    {
+        for (int i = treasureCount; i > 0; --i) {
+            GameObject treasure = Instantiate(treasurePrefab, transform);
+            Vector2 p = Random.insideUnitCircle * 100.0f;
+            treasure.transform.localPosition = new Vector3(p.x, 0.0f, p.y);
+
+            // Enter/Exit visibility hitbox
+            treasure.GetComponent<Hideable>().SetVisible(false);
+            treasure.GetComponent<Collider>().enabled = false;
+            treasure.GetComponent<Collider>().enabled = true;
+        }
+    }
+}
