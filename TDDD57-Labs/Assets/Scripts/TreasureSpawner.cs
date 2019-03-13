9a2c2dd7c65b69c7fa20aab9f99e24e826cba7ae
Axel Nordanskog <axeno840@student.liu.se>
2019-03-13 10:54:28 +0100

    Guarantee at least 3 initially visible treasures

--- TDDD57-Labs/Assets/Scripts/TreasureSpawner.cs
+++ TDDD57-Labs/Assets/Scripts/TreasureSpawner.cs
@@ -6,19 +6,34 @@ public class TreasureSpawner : MonoBehaviour
 {
     public GameObject treasurePrefab;
 
-    private int treasureCount = 10;
-    
+    private int treasureCountInner = 3;
+    private int treasureCountOuter = 7;
+
+    private float innerRadius = 30.0f;
+    private float outerRadius = 100.0f;
+
     void Start()
     {
-        for (int i = treasureCount; i > 0; --i) {
-            GameObject treasure = Instantiate(treasurePrefab, transform);
-            Vector2 p = Random.insideUnitCircle * 100.0f;
-            treasure.transform.localPosition = new Vector3(p.x, 0.0f, p.y);
-
-            // Enter/Exit visibility hitbox
-            treasure.GetComponent<Hideable>().SetVisible(false);
-            treasure.GetComponent<Collider>().enabled = false;
-            treasure.GetComponent<Collider>().enabled = true;
+        for (int i = treasureCountInner; i > 0; --i)
+        {
+            Vector2 localPosition = Random.insideUnitCircle * innerRadius;
+            SpawnTreasure(localPosition);
         }
+        for (int i = treasureCountOuter; i > 0; --i)
+        {
+            Vector2 localPosition = Random.insideUnitCircle * outerRadius;
+            SpawnTreasure(localPosition);
+        }
+    }
+
+    private void SpawnTreasure(Vector2 localPosition)
+    {
+        GameObject treasure = Instantiate(treasurePrefab, transform);
+        treasure.transform.localPosition = new Vector3(localPosition.x, 0.0f, localPosition.y);
+
+        // Enter/Exit visibility hitbox
+        treasure.GetComponent<Hideable>().SetVisible(false);
+        treasure.GetComponent<Collider>().enabled = false;
+        treasure.GetComponent<Collider>().enabled = true;
     }
 }
