f46ca2d78c4acfab8b72c0ef3e19d39cd33c4d02
Erik Mansén <erima668@student.liu.se>
2019-03-13 11:41:08 +0100

    Fix Treasure selection indicator rotation

--- TDDD57-Labs/Assets/Scripts/Treasure.cs
+++ TDDD57-Labs/Assets/Scripts/Treasure.cs
@@ -29,20 +29,6 @@ public class Treasure : MonoBehaviour {
     }
 
     void UpdateSelectionMarker() {
-        selectionIndicator.transform.right = Camera.main.transform.right;
-    }
-
-    private void ShowAndroidToastMessage(string message) {
-        AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject unityActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-
-        if (unityActivity != null) {
-            AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
-            unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() => {
-                AndroidJavaObject toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", unityActivity,
-                    message, 0);
-                toastObject.Call("show");
-            }));
-        }
+        selectionIndicator.transform.rotation = Quaternion.LookRotation(transform.position - Camera.main.transform.position);
     }
 }
